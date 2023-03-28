2bb8280b9350e5b660ab131ce2923c6a3d6891fa
Robert Lowe <rlowe@semo.edu>
2023-03-28 13:42:29 -0500

    Multi-Dimensional array support

--- 08 - Collections and User Defined Types/calc/calc.py
+++ 08 - Collections and User Defined Types/calc/calc.py
@@ -5,11 +5,61 @@ import sys
 from enum import Enum, auto
 from CalcLexer import Lexer,Token
 from CalcParser import Parser,Operator
+import copy
+
+class CalcArray:
+    def __init__(self, bounds, ref_type):
+        """
+        Construct an array for the calc language.
+        bounds - List of tuples (lbound, ubound)
+        ref_type - RefType enumeration field
+        """
+        self.bounds = bounds
+        self.ref_type = ref_type
+        self.data = None
+        for bound in bounds[::-1]:
+            n = bound[1] - bound[0] + 1
+            if self.data == None:
+                self.data = [0] * n
+            else:
+                self.data = [copy.deepcopy(self.data) for i in range(n)]
+
+    def get_enclosing_list(self, index):
+        bsize = len(index)
+        ar = self.data
+
+        # get the enclosing list
+        for i in range(bsize-1):
+            # subtract the lower bound to make this zero based
+            j = index[i] - self.bounds[i][0]
+            ar = ar[j]
+        return ar
+
+
+    def get(self, index):
+        """
+        index - a tuple of integers forming the index to the array
+        """
+        #get the item
+        ar = self.get_enclosing_list(index)
+        return ar[index[-1] - self.bounds[-1][0]]
+
+
+    def set(self, index, value):
+        """
+        index - a tuple of integers forming the index to the array
+        """
+        #set the item
+        ar = self.get_enclosing_list(index)
+        ar[index[-1] - self.bounds[-1][0]] = value
+
+
 
 class RefType(Enum):
     INT_VAR = auto()
     REAL_VAR = auto()
     ARRAY_VAR = auto()
+
 
 class RefEntry:
     def __init__(self, value,ref_type):
@@ -74,6 +124,10 @@ def eval_tree(tree, env):
         return eval_input(tree, env)
     elif tree.op == Operator.DECL:
         return eval_decl(tree, env)
+    elif tree.op == Operator.ARRAY_DECL:
+        return eval_array_decl(tree, env)
+    elif tree.op == Operator.ARRAY_VAR:
+        return eval_array_var(tree, env)
 
 def eval_program(tree, env):
     # semantic behavior for now is we print the result of every statement
@@ -125,6 +179,11 @@ def eval_var(tree, env):
         runtime_error(tree, f"Undefined Variable '{tree.token.lexeme}'")
     return val.value
 
+def eval_array_var(tree, env):
+    ar = eval_var(tree, env)
+    index = get_array_index(tree, env)
+    return ar.get(index)
+
 
 def eval_input(tree, env):
     try:
@@ -137,9 +196,11 @@ def eval_input(tree, env):
         x = input(f"{name}=")
         
         if var.ref_type == RefType.INT_VAR:
-            var.value = int(x)
+            x = int(x)
         elif var.ref_type==RefType.REAL_VAR:
-            var.value  = float(x)
+            x = float(x)
+        
+        assign(tree.children[0], x, env)
     except:
         runtime_error(tree, "Invalid Input")
 
@@ -161,8 +222,7 @@ def eval_assign(tree, env):
     elif var.ref_type == RefType.REAL_VAR:
         value = float(value)
 
-    var.value = value
-
+    assign(tree.children[0], value, env)
 
 def eval_decl(tree, env):
     # get the type
@@ -173,18 +233,59 @@ def eval_decl(tree, env):
         ref_type = RefType.REAL_VAR
         init = 0.0
 
-    # get the name
+    # get the name and the value
     name = tree.children[0].token.lexeme
+    value = RefEntry(init, ref_type)
 
+    # insert into our env
+    declare_name(name, value, env)
+
+
+def eval_array_decl(tree, env):
+    # get the array parameters
+    ref_type = tree.token.token
+    bounds = tree.children[0].children
+    name = tree.children[1].token.lexeme
+
+    # convert the bound list
+    bound_list = []
+    for i in range(0, len(bounds), 2):
+        bound_list.append((bounds[i].token.value, bounds[i+1].token.value))
+    
+    # construct the array
+    ar = CalcArray(bound_list, ref_type)
+
+    # attempt to insert the array
+    value = RefEntry(ar, RefType.ARRAY_VAR)
+    declare_name(name, value, env)
+
+
+################ Helper Functions ########################
+def assign(tree, value, env):
+    if tree.op == Operator.VAR:
+        assign_var(tree, value, env)
+    elif tree.op == Operator.ARRAY_VAR:
+        assign_array_var(tree, value, env)
+    
+def assign_var(tree, value, env):
+    env.get(tree.token.lexeme).value = value
+
+def assign_array_var(tree, value, env):
+    ar = env.get(tree.token.lexeme).value
+    index = get_array_index(tree, env)
+    ar.set(index, value)
+
+def get_array_index(tree, env):
+    index = []
+    for t in tree.children:
+        index.append(eval_tree(t, env))
+    return index
+
+def declare_name(name, value, env):
     # make sure the name is unique
     if env.get(name) != None:
         runtime_error(tree, f"Redeclaration of variable {name}")
-
-    # insert into our environment
-    value = RefEntry(init, ref_type)
     env.set(name, value)
-
-    
 
 
 def runtime_error(tree, msg):
